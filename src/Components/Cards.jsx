8e17682d8e5eeea675b7b6fb65e587287b165e8e
prabesh <prabeshdace@gmail.com>
2023-02-12 23:16:12 +0545

    rendered products in products page     Used fakestoreAPI for products

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,23 +1,26 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import { Rating } from "@mui/material";
 import shoee from "../Assets/shoee.jpg";
 
-const Cards = () => {
-  let products = require("../Utils/products.json");
+const Cards = (props) => {
+  //let products = require("../Utils/products.json");
 
+  
   return (
-    <div className=" gap-y-20 gap-x-8 grid mobile:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-4">
-      {products.products.map((product) => {
+    <div className="">
+      
    
-        return (
-          <div key={product.id} className="card">
-            <img className="w-full h-[20rem] object-cover" src={shoee} />
+        
+          <div key={props.id} className="card h-[40rem]">
+            <img className="w-full h-[20rem] object-contain" src={props.image} />
 
-            <div className="shadow-2xl rounded-lg px-2 py-2">
-              <h3 className="text-2xl">{product.title}</h3>
+            <div className="shadow-[-2px_-2px_10px_rgba(255,255,255,1),3px_3px_10px_rgba(0,0,0,0.2)] rounded-lg px-2 py-2">
+         
+              <h3 className="text-2xl truncate">{props.title}</h3>
+           
               <div className="flex flex-row py-3 my-2">
                 <h2 className="text-lg font-medium">
-                  {product.price}
+                  {props.price}
                 </h2>
               </div>
               <div className="-mt-5 flex flex-row">
@@ -25,13 +28,14 @@ const Cards = () => {
                 <h6 className="ml-4 text-gray-500">10 reviews</h6>
               </div>
 
+
               <div className="flex justify-center m-6 ">
                 <button className="bg-secondary shadow-[-2px_-2px_10px_rgba(255,255,255,1),3px_3px_10px_rgba(0,0,0,0.2)] active:shadow-[inset_-2px_-2px_5px_rgba(255,255,255,0.7),inset_3px_3px_5px_rgba(0,0,0,0.1)] hover:bg-white w-full p-3 mr-5 mb-4 rounded-full ">Buy Now</button>
               </div>
             </div>
           </div>
-        );
-      })}
+        
+      
     </div>
   );
 };
