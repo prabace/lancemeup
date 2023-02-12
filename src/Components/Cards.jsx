3d9177b0b822f06efde51ad8f67f778aa911d0b0
prabesh <prabeshdace@gmail.com>
2023-02-12 11:48:41 +0545

    grid layout on product page

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -6,7 +6,7 @@ const Cards = () => {
   let products = require("../Utils/products.json");
 
   return (
-    <div>
+    <div className=" gap-y-20 gap-x-8 grid mobile:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-4">
       {products.products.map((product) => {
    
         return (
@@ -26,7 +26,7 @@ const Cards = () => {
               </div>
 
               <div className="flex justify-center m-6 ">
-                <button>Buy Now</button>
+                <button className="bg-secondary shadow-[-2px_-2px_10px_rgba(255,255,255,1),3px_3px_10px_rgba(0,0,0,0.2)] active:shadow-[inset_-2px_-2px_5px_rgba(255,255,255,0.7),inset_3px_3px_5px_rgba(0,0,0,0.1)] hover:bg-white w-full p-3 mr-5 mb-4 rounded-full ">Buy Now</button>
               </div>
             </div>
           </div>
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -4,8 +4,9 @@ import Cards from '../Components/Cards'
 const Product = () => {
 
   return (
-    <div className='m-20 bg-white grid gap-y-20 mobile:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-4'>
-        <Cards/>     
+    <div className='m-20 bg-white'>
+        <Cards/>    
+         
         
     </div>
   )
